764567ad5f2caa78d3e36ca73cbe36aa63a0f7fb
ChloeArd <chloe.ardoise@gmail.com>
2020-11-25 21:04:42 +0100

    Exercice 53

--- app.js
+++ app.js
@@ -1,32 +1,130 @@
-let paragraphe = document.querySelector("p");
+let paragraphe = document.getElementById("p1");
 paragraphe.style.color = "green";
 paragraphe.style.fontSize = "1rem";
 console.log(paragraphe);
 
-let nouveauText = document.getElementById("newText");
-nouveauText.innerText = " Fin du Paragraphe Lorem Ipsum";
-nouveauText.style.animationName = "animation";
-nouveauText.style.animationDuration = "10s";
-console.log(nouveauText);
+let paragraphe2 = document.getElementById("p2");
+paragraphe2.style.color = "green";
+paragraphe2.style.fontSize = "1rem";
+console.log(paragraphe2);
 
-let para2 = document.getElementById("anim2");
-para2.style.animationName = "animation";
-para2.style.animationDuration = "10s";
-console.log(para2);
+let paragraphe3 = document.getElementById("p3");
+paragraphe3.style.color = "green";
+paragraphe3.style.fontSize = "1rem";
+console.log(paragraphe3);
 
-let span = document.querySelector(".spanTest");
-span.style.backgroundColor = "white";
-span.style.display = "block";
-span.dir = "rtl";
-console.log(span);
+let paragraphe4 = document.getElementById("p4");
+paragraphe4.style.color = "green";
+paragraphe4.style.fontSize = "1rem";
+paragraphe4.style.animationName = "animation";
+paragraphe4.style.animationDuration = "10s";
+console.log(paragraphe4);
+
+let paragraphe5 = document.getElementById("p5");
+paragraphe5.style.color = "green";
+paragraphe5.style.fontSize = "1rem";
+paragraphe5.innerText = " Fin du Paragraphe Lorem Ipsum";
+paragraphe5.style.animationName = "animation";
+paragraphe5.style.animationDuration = "10s";
+console.log(paragraphe5);
+
+let span1 = document.querySelector(".spanTest1");
+span1.style.backgroundColor = "white";
+span1.style.display = "block";
+span1.dir = "rtl";
+console.log(span1);
+
+let span2 = document.querySelector(".spanTest2");
+span2.style.backgroundColor = "white";
+span2.style.display = "block";
+span2.dir = "rtl";
+console.log(span2);
+
+let span3 = document.querySelector(".spanTest3");
+span3.style.backgroundColor = "white";
+span3.style.display = "block";
+span3.dir = "rtl";
+console.log(span3);
+
+let span4 = document.querySelector(".spanTest4");
+span4.style.backgroundColor = "white";
+span4.style.display = "block";
+span4.dir = "rtl";
+console.log(span4);
+
+let span5 = document.querySelector(".spanTest5");
+span5.style.backgroundColor = "white";
+span5.style.display = "block";
+span5.dir = "rtl";
+console.log(span5);
+
+let span6 = document.querySelector(".spanTest6");
+span6.style.backgroundColor = "white";
+span6.style.display = "block";
+span6.dir = "rtl";
+console.log(span6);
+
+let span7 = document.querySelector(".spanTest7");
+span7.style.backgroundColor = "white";
+span7.style.display = "block";
+span7.dir = "rtl";
+console.log(span7);
+
+let span8 = document.querySelector(".spanTest8");
+span8.style.backgroundColor = "white";
+span8.style.display = "block";
+span8.dir = "rtl";
+console.log(span8);
+
+let span9 = document.querySelector(".spanTest9");
+span9.style.backgroundColor = "white";
+span9.style.display = "block";
+span9.dir = "rtl";
+console.log(span9);
+
+let span10 = document.querySelector(".spanTest10");
+span10.style.backgroundColor = "white";
+span10.style.display = "block";
+span10.dir = "rtl";
+console.log(span10);
 
 let a = document.querySelector('a');
-let lien = document.createTextNode("Mon texte");
-a.appendChild(lien);
+let lien1 = document.createTextNode("Mon texte");
+a.appendChild(lien1);
 a.href = "https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal";
 document.body.appendChild(a);
 console.log(a);
-console.log(lien);
+console.log(lien1);
+
+let lien2 = document.createTextNode("Mon texte 2");
+a.appendChild(lien2);
+a.href = "https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal";
+document.body.appendChild(a);
+console.log(a);
+console.log(lien2);
+
+let lien3 = document.createTextNode("Mon texte 3");
+a.appendChild(lien3);
+a.href = "https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal";
+document.body.appendChild(a);
+console.log(a);
+console.log(lien3);
+
+let lien4 = document.createTextNode("Mon texte 4");
+a.appendChild(lien4);
+a.href = "https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal";
+document.body.appendChild(a);
+console.log(a);
+console.log(lien4);
+
+let lien5 = document.createTextNode("Mon texte 5");
+a.appendChild(lien5);
+a.href = "https://fr.wikipedia.org/wiki/Wikip%C3%A9dia:Accueil_principal";
+document.body.appendChild(a);
+console.log(a);
+console.log(lien5);
+
+
 
 
 
